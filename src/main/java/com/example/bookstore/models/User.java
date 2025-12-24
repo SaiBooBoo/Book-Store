3872e5223fa242e406faecae42bd6ed4c9e10e2b
saishanzinmao@startinpoint.com <saishanzinmao@startinpoint.com>
2025-12-24 16:58:13 +0630

    latest version of bookstore

--- src/main/java/com/example/bookstore/models/User.java
+++ src/main/java/com/example/bookstore/models/User.java
@@ -1,7 +1,9 @@
 package com.example.bookstore.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -9,50 +11,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
+
+    @Column(nullable = false, unique= true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
 
     public User() {
 
-    }
-
-    public User(Long id, String firstName, String lastName, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
